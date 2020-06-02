0fbea7d853e3cb66bce65063c0d34123325308f3
Minakshi Verma <mvedwan@gmail.com>
2020-06-02 18:57:33 -0400

    "exercise 00- exercise 14 completed"

--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -35,6 +35,8 @@ waypoints = [
 
 # Add a new waypoint to the list
 # YOUR CODE HERE
+waypoints.append({"lat": 23, "lon": -130, "name":"test place"})
+print(waypoints)
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
@@ -42,6 +44,16 @@ waypoints = [
 # waypoints list.
 
 # YOUR CODE HERE
+# waypoints[0]= [{"lon": 130, "name":"not a real name "}]
+# waypoints[0].update("lon": 130, "name":"not a real name")
+for item in waypoints:
+    if item["name"]== "a place":
+        item["name"]= "not a real name"
+    if item["lon"]== -121:
+        item["lon"]= -130
+print(waypoints)
 
 # Write a loop that prints out all the field values for all the waypoints
 # YOUR CODE HERE
+for item in waypoints:
+    print(item)
--- src/11_args.py
+++ src/11_args.py
@@ -5,6 +5,8 @@
 # the sum. This is what you'd consider to be a regular, normal function.
 
 # YOUR CODE HERE
+def f1(a,b):
+    return a+b
 
 print(f1(1, 2))
 
@@ -14,6 +16,9 @@ print(f1(1, 2))
 
 # YOUR CODE HERE
 
+def f2(*args):
+    return sum(args)
+  
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
 print(f2(1, 4, -12))            # Should print -7
@@ -30,6 +35,12 @@ print(f2(a))    # Should print 22
 # Note: Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+def f3(args):
+    for arg in args:
+        if (len(str(arg)))==1:
+            return arg
+        if (len(str(arg)))==2:
+            return sum(arg) 
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
